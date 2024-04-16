dd343003480beb5c56bc9f7ba885002722729963
Jan K. <kleinjan826@gmail.com>
2024-04-16 23:00:53 +0200

    added needed features

---
+++ ToDoList/ToDoList/FrmToDo.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace ToDoList
+{
+    partial class FrmToDo
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            lbxTasks = new ListBox();
+            btnAddTask = new Button();
+            tbxAddTask = new TextBox();
+            tmrToDo = new System.Windows.Forms.Timer(components);
+            lblHeadline = new Label();
+            btnDarkMode = new Button();
+            btnExplain = new Button();
+            SuspendLayout();
+            // 
+            // lbxTasks
+            // 
+            lbxTasks.FormattingEnabled = true;
+            lbxTasks.Location = new Point(199, 54);
+            lbxTasks.Name = "lbxTasks";
+            lbxTasks.Size = new Size(427, 244);
+            lbxTasks.TabIndex = 0;
+            lbxTasks.DoubleClick += lbxTasks_DoubleClick;
+            lbxTasks.KeyDown += lbxTasks_KeyDown;
+            // 
+            // btnAddTask
+            // 
+            btnAddTask.Location = new Point(532, 317);
+            btnAddTask.Name = "btnAddTask";
+            btnAddTask.Size = new Size(94, 29);
+            btnAddTask.TabIndex = 1;
+            btnAddTask.Text = "Hinzufügen";
+            btnAddTask.UseVisualStyleBackColor = true;
+            btnAddTask.Click += btnAddTask_Click;
+            // 
+            // tbxAddTask
+            // 
+            tbxAddTask.Location = new Point(199, 317);
+            tbxAddTask.Name = "tbxAddTask";
+            tbxAddTask.Size = new Size(286, 27);
+            tbxAddTask.TabIndex = 2;
+            tbxAddTask.KeyDown += tbxAddTask_KeyDown;
+            // 
+            // lblHeadline
+            // 
+            lblHeadline.AutoSize = true;
+            lblHeadline.Location = new Point(196, 21);
+            lblHeadline.Name = "lblHeadline";
+            lblHeadline.Size = new Size(74, 20);
+            lblHeadline.TabIndex = 3;
+            lblHeadline.Text = "Aufgaben";
+            // 
+            // btnDarkMode
+            // 
+            btnDarkMode.Location = new Point(12, 12);
+            btnDarkMode.Name = "btnDarkMode";
+            btnDarkMode.Size = new Size(107, 29);
+            btnDarkMode.TabIndex = 4;
+            btnDarkMode.Text = "Dark Mode";
+            btnDarkMode.UseVisualStyleBackColor = true;
+            btnDarkMode.Click += btnDarkMode_Click;
+            // 
+            // btnExplain
+            // 
+            btnExplain.Location = new Point(666, 12);
+            btnExplain.Name = "btnExplain";
+            btnExplain.Size = new Size(94, 49);
+            btnExplain.TabIndex = 5;
+            btnExplain.Text = "Erklärung Steuerung";
+            btnExplain.UseVisualStyleBackColor = true;
+            btnExplain.Click += btnExplain_Click;
+            // 
+            // FrmToDo
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnExplain);
+            Controls.Add(btnDarkMode);
+            Controls.Add(lblHeadline);
+            Controls.Add(tbxAddTask);
+            Controls.Add(btnAddTask);
+            Controls.Add(lbxTasks);
+            Name = "FrmToDo";
+            Text = "ToDos";
+            Load += FrmToDo_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox lbxTasks;
+        private Button btnAddTask;
+        private TextBox tbxAddTask;
+        private System.Windows.Forms.Timer tmrToDo;
+        private Label lblHeadline;
+        private Button btnDarkMode;
+        private Button btnExplain;
+    }
+}
---
+++ ToDoList/ToDoList/FrmToDo.cs
@@ -0,0 +1,139 @@
+using System.ComponentModel;
+using System.Xml.Linq;
+
+namespace ToDoList
+{
+    public partial class FrmToDo : Form
+    {
+        public static BindingList<string> TasksList = new BindingList<string>();
+        private bool isDarkMode = false;
+        public FrmToDo()
+        {
+            InitializeComponent();
+        }
+        private void FrmToDo_Load(object sender, EventArgs e)
+        {
+
+            lbxTasks.DataSource = TasksList;
+
+            ToolTip toolTipbtnAddTask = new ToolTip();
+            toolTipbtnAddTask.SetToolTip(btnAddTask, "Geben Sie links in die TextBox die Aufgabe ein, die Sie hinzufügen möchten.");
+            ToolTip toolTipbtnDarkMode = new ToolTip();
+            toolTipbtnDarkMode.SetToolTip(btnDarkMode, "Durch drücken wechseln Sie den Ansichtsmodus.");
+            ToolTip toolTipbtnExplain = new ToolTip();
+            toolTipbtnExplain.SetToolTip(btnExplain, "Durch drücken öffnen Sie die Funktionen und Steuerung Erklärung der Software.");
+
+        }
+        private void AddTask()
+        {
+            if (!string.IsNullOrWhiteSpace(tbxAddTask.Text))
+            {
+                bool taskAlreadyExists = false;
+                foreach (var task in TasksList)
+                {
+                    if (task == tbxAddTask.Text)
+                    {
+                        taskAlreadyExists = true;
+                        break;
+                    }
+                }
+                if (!taskAlreadyExists)
+                {
+                    TasksList.Add(tbxAddTask.Text);
+                    tbxAddTask.Text = "";
+                }
+                else
+                {
+                    MessageBox.Show("Die Aufgabe existiert bereits!");
+                }
+            }
+        }
+
+        private void btnAddTask_Click(object sender, EventArgs e)
+        {
+            AddTask();
+
+        }
+        
+
+
+        private void lbxTasks_DoubleClick(object sender, EventArgs e)
+        {
+            if (lbxTasks.SelectedItem != null)
+            {
+                string newTaskDescription = Microsoft.VisualBasic.Interaction.InputBox("Bitte geben Sie die korrigierte Aufgabe ein:", "Aufgabe bearbeiten", "");
+
+                if (!string.IsNullOrWhiteSpace(newTaskDescription))
+                {
+                    TasksList[lbxTasks.SelectedIndex] = newTaskDescription;
+                }
+            }
+        }
+
+        private void lbxTasks_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                if (lbxTasks.SelectedItem != null)
+                {
+                    TasksList.RemoveAt(lbxTasks.SelectedIndex);
+                }
+            }
+        }
+
+        private void btnDarkMode_Click(object sender, EventArgs e)
+        {
+            isDarkMode = !isDarkMode;
+            if (isDarkMode)
+            {
+                BackColor = Color.FromArgb(31, 31, 31);
+                ForeColor = Color.White;
+                lbxTasks.BackColor = Color.FromArgb(45, 45, 45);
+                lbxTasks.ForeColor = Color.White;
+
+                tbxAddTask.BackColor = Color.FromArgb(45, 45, 45);
+                tbxAddTask.ForeColor = Color.White;
+                btnAddTask.BackColor = Color.FromArgb(45, 45, 45);
+                btnAddTask.ForeColor = Color.White;
+                btnDarkMode.BackColor = Color.FromArgb(45, 45, 45);
+                btnDarkMode.ForeColor = Color.White;
+                btnDarkMode.Text = "Light Mode";
+
+            }
+            else
+            {
+                BackColor = SystemColors.Control;
+                ForeColor = SystemColors.ControlText;
+                lbxTasks.BackColor = SystemColors.Window;
+                lbxTasks.ForeColor = SystemColors.WindowText;
+
+                tbxAddTask.BackColor = SystemColors.Window;
+                tbxAddTask.ForeColor = SystemColors.ControlText;
+                btnAddTask.BackColor = SystemColors.Control;
+                btnAddTask.ForeColor = SystemColors.ControlText;
+                btnDarkMode.BackColor = SystemColors.Control;
+                btnDarkMode.ForeColor = SystemColors.ControlText;
+                btnDarkMode.Text = "Dark Mode";
+            }
+        }
+
+        private void btnExplain_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Willkommen bei der ToDo-Liste!\n\n" +
+                    "Hier sind die Funktionen der Software:\n\n" +
+                    "- Aufgaben hinzufügen: Geben Sie eine Aufgabe in das Textfeld ein und klicken Sie auf 'Aufgabe hinzufügen'.\n\n" +
+                    "- Aufgaben bearbeiten: Doppelklicken Sie auf eine Aufgabe in der Liste, um sie zu bearbeiten. Geben Sie dann die korrigierte Aufgabe ein und bestätigen Sie.\n\n" +
+                    "- Aufgaben löschen: Wählen Sie eine Aufgabe aus der Liste aus und drücken Sie die 'Entf'-Taste auf Ihrer Tastatur.\n\n" +
+                    "- Ansichtsmodus ändern: Klicken Sie auf 'Dark Mode', um zwischen dem dunklen und hellen Modus zu wechseln.\n\n" +
+                    "Vielen Dank, dass Sie die ToDo-Liste verwenden!");
+        }
+
+        private void tbxAddTask_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                AddTask();
+            }
+        }
+    }
+}
